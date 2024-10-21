3ef840b87eecee592e1ad2c9413f890078da16ec
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2024-10-21 11:02:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,36 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+funtion loadurl(url){
+	return new Promise((res,rej)=>{
+		fetch(url)
+		.then(response=>{
+			if(!response.ok){
+				throw new Error()
+			}
+			return response.blob();
+		})
+		.then(blob=>{
+			let img=URL.createObjectURL(blob);
+			res(img);
+		})
+		.catch(e=>{
+			rej(`Failed to load image's URL: ${url}`);
+		})
+	})
+}
+function download(){
+	let imagePro=images.map(image=>loadurl(image.url));
+	Promise.all(imagePro)
+	.then(img=>{
+		img.forEach(img=>{
+			let imageElement=document.createElement("img");
+			imageElement.src=img;
+		output.appendChild(imageElement);
+		})
+	})
+	.catch(e=>{
+		console.log(e)
+	})
+}
+btn.addEventListener("click",download);
